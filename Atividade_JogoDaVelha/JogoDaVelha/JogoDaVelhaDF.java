cf7f0ac4b79294b2a52492bce794acbaadf48ad7
Gilmar Araújo Santos Junior <araujogilmarjr@gmail.com>
2025-10-22 17:31:10 -0300

    Add files via upload

---
+++ Atividade_JogoDaVelha/JogoDaVelha/JogoDaVelhaDF.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class JogoDaVelhaDF {
+    private static char[][] tabela = new char[3][3];
+    private static char jogadoratual = 'x';
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+
+    }
+
+}
